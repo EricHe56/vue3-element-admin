348667e30eb77e6a3ee288917c41ea4291ccc27d
EH <eric.he@tinycloud.uk>
2025-02-25 23:51:44 +0000

    chore: pnpm_run_dev.bat

---
+++ pnpm_run_dev.bat
@@ -0,0 +1,7 @@
+call nvs use 22.12.0
+set VUE_APP_COS_BUCKET=your-bucket-name
+set VUE_APP_COS_REGION=ap-shanghai
+set VUE_APP_COS_WEB_ROOT=https://your-bucket-name.cos.ap-shanghai.myqcloud.com/
+set VUE_APP_BASE_API=/web-api
+
+pnpm run dev
